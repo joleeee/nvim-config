457b6ca3c8b7c29428d777572307f889dc6f447d
jole <jonas@jole.xyz>
2022-11-09 23:57:38 +0100

    move from deprecated function

--- lsp/init.lua
+++ lsp/init.lua
@@ -22,7 +22,7 @@ vim.diagnostic.config({
 })
 
 -- Add additional capabilities supported by nvim-cmp
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local lspconfig = require("lspconfig")
 
